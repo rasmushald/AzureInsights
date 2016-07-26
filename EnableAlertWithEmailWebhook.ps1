f670f9a83ecc1af93f88ca4684d5b43a9184bcc8
rasmushald <rasmus_hald@hotmail.com>
2016-07-26 21:01:57 +0200

    Cleaned up

--- EnableAlertWithEmailWebhook.ps1
+++ EnableAlertWithEmailWebhook.ps1
@@ -1,19 +1,16 @@
-﻿#Configure Alert Rule
+#Configure Alert Rule
+
+# Azure RG Deploy
+$RGName = <RG>
+$Location = "Central US"
  
 #Parameters
-$Email = New-AzureRmAlertRuleEmail -CustomEmail rasmush@microsoft.com
-$Webhook = New-AzureRmAlertRuleWebhook -ServiceUri https://s2events.azure-automation.net/webhooks?token=GSinbmPPJBWt1NvaYrvrEsBLLXGQ9gbMMHMJFq0fhho%3d
+$Email = New-AzureRmAlertRuleEmail -CustomEmail you@domain.com
+$Webhook = New-AzureRmAlertRuleWebhook -ServiceUri <webhook url>
  
 # Create Alert Rule 
-Add-AzureRmLogAlertRule -Name "Alert Stop" -Location "North Europe" -ResourceGroup AzureInsights_Demo002 -OperationName microsoft.web/sites/stop/action -Status Succeeded -TargetResourceGroup AzureInsights_Demo002 -Actions $Email, $Webhook
+Add-AzureRmLogAlertRule -Name "Alert Stop" -Location $Location -ResourceGroup $RGName -OperationName microsoft.web/sites/stop/action -Status Succeeded -TargetResourceGroup $RGName -Actions $Email, $Webhook
  
 # Get Alert Rule
-Get-AzureRmAlertRule -ResourceGroup AzureInsights_Demo002 
+Get-AzureRmAlertRule -ResourceGroup $RGName
 
-#---More---#
-
-#Get Metric Definitions
-Get-AzureRmMetricDefinition -ResourceId /subscriptions/a64cb62c-cf2d-4f05-8c5b-257cdbc05bad/resourcegroups/AzureInsights_Demo002/providers/Microsoft.Web/sites/webSitegnwfqqbicki32 
-
-#Get active rules
-Get-AzureRmAlertRule -ResourceGroup AzureInsights_Demo002 -TargetResourceId /subscriptions/a64cb62c-cf2d-4f05-8c5b-257cdbc05bad/resourcegroups/AzureInsights_Demo002/providers/Microsoft.Web/sites/webSitegnwfqqbicki32
